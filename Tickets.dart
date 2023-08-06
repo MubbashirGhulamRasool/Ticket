58e348b0bd288aa27fb1392641f9abdbbc6af988
MubbashirGhulamRasool <appstechpk@gmail.com>
2023-08-06 11:25:09 +0500

    first commit

--- Tickets.dart
+++ Tickets.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:math';
+
 bool programmStart = true;
+List<Map> ticketTable =[{}];
 
 class Ticket {
   int ticketNumber;
@@ -13,7 +15,8 @@ class Ticket {
   static int generateTicketNumber() {
     // For simplicity, you can use the current date and time as the ticket number
     DateTime now = DateTime.now();
-    String ticketStr = "${now.year}${now.month}${now.day}${now.hour}${now.minute}${now.second}";
+    String ticketStr =
+        "${now.year}${now.month}${now.day}${now.hour}${now.minute}${now.second}";
     return int.parse(ticketStr);
   }
 }
@@ -25,84 +28,72 @@ Ticket generateTicket(String customerName) {
   return Ticket(ticketNumber, issueDate, customerName);
 }
 
-login(name,email)
-{
- if (name == "admin" && email == "admin@gmail.com") {
+login(email, password) {
+  if (email == "admin@gmail.com" && password == "1234") {
     print("Admin Login Success");
     programmStart = false;
     return true;
-  } 
-  else if(name == "user" && email == "1234"){
+  } else if (email == "user" && password == "1234") {
     print("User Login Success");
     programmStart = false;
     return true;
-    
-  }
-  else {
-     print("Login Fail");
-     programmStart = false;
-     return false;
-     
-
+  } else {
+    print("Login Fail");
+    programmStart = false;
+    return false;
   }
 }
 
 void main() {
+  print("Enter Your User Email");
+  String userEmail = stdin.readLineSync()!;
+  print("Enter Your User Password");
+  String userPassword = stdin.readLineSync()!;
 
-print("Enter Your User Name"); 
-String userName = stdin.readLineSync()!;
- print("Enter Your User Email");
-String userEmail = stdin.readLineSync()!;
+  while (programmStart == true) {
+    bool loginSuccess = login(userEmail, userPassword);
+    if (loginSuccess == true) {
+      print("Add New Ticket Press 1");
+      print("View Ticket Press 2");
+      print("Delete Ticket Press 3");
+      print("Update New Ticket Press 4");
 
-while (programmStart == true) {
+      int option = int.parse(stdin.readLineSync()!);
+      switch (option) {
+        case 1:
+          print("Hello 1 $option");
+          print("Enter Your Customer Name");
+          String customerName = stdin.readLineSync()!;
+          // String customerName = "John Doe";
+          Ticket newTicket = generateTicket(customerName);
 
-bool loginSuccess = login(userName, userEmail);
-if (loginSuccess == true) {
+          // print("Ticket Number: ${newTicket.ticketNumber}");
+          // print("Issue Date: ${newTicket.issueDate}");
+          // print("Customer Name: ${newTicket.customerName}");
 
+          // List ticketList = [];
 
-  print("Add New Ticket Press 1");
-  print("View Ticket Press 2");
-  print("Delete Ticket Press 3");
-  print("Update New Ticket Press 4");
-  
-  int option = int.parse(stdin.readLineSync()!);
-  switch (option) {
-    case 1:
-      print("Hello 1 $option");
-      break;
-       case 2:
-      print("Hello 2 $option");
-      break;
-    default:
+          List<Map> ticketTable = [
+            {
+              'ticketNumber': newTicket.ticketNumber,
+              'issueDate': newTicket.issueDate,
+              'customerName': newTicket.customerName,
+              'book': true
+            },
+          ];
+
+          // usersEligibility.add({'ticketNumber': 164984498,'issueDate':2023-08-06,'customerName':'Ali', 'book': true});
+
+          print('Customer Book: $ticketTable');
+          break;
+        case 2:
+          print("Hello 2 $option");
+          print('Customer Book: $ticketTable');
+          break;
+        default:
+      }
+    } else {
+      print('Login Fail');
+    }
   }
-
-  print("Enter Your Customer Name"); 
-  String customerName = stdin.readLineSync()!;
-  // String customerName = "John Doe";
-  Ticket newTicket = generateTicket(customerName);
-
-  // print("Ticket Number: ${newTicket.ticketNumber}");
-  // print("Issue Date: ${newTicket.issueDate}");
-  // print("Customer Name: ${newTicket.customerName}");
-
-
-    List ticketList = [];
-
-    List<Map> ticketTable = [
-    {'ticketNumber': newTicket.ticketNumber,'issueDate':newTicket.issueDate,'customerName':newTicket.customerName, 'book': true},
-  ];
-
-  // usersEligibility.add({'ticketNumber': 164984498,'issueDate':2023-08-06,'customerName':'Ali', 'book': true});
-
-  print('Customer Book: $ticketTable');
-} else {
-  print('Login Fail');
-}
-  
-}
-
-
-
-  
-
 }
